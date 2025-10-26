f8f5f26c314286f6be360a2ce93688809997b1ad
Dharmender Singh <51146901+suthwald@users.noreply.github.com>
2025-10-26 18:21:50 +0530

    Enable agent setup and execution code
    Uncommented code for agent setup and execution.

--- simple_calculator_exl/google-adk-tutorial/agent.py
+++ simple_calculator_exl/google-adk-tutorial/agent.py
@@ -48,3 +48,70 @@ root_agent = LlmAgent(
     model="gemini-2.5-flash",  # Use a reliable model
     tools=[get_current_time, get_search_tool()],
 )
+
+
+
+
+
+# from google.adk import Agent, Runner
+# from google.adk.sessions import InMemorySessionService
+# from google.adk.tools.langchain_tool import LangchainTool
+# from google.genai import types
+# from langchain_community.tools import DuckDuckGoSearchRun
+# import asyncio
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# APP_NAME = "news_app"
+# USER_ID = "1234"
+# SESSION_ID = "session1234"
+
+# # Wrap with LangchainTool
+# adk_tavily_tool = LangchainTool(tool=DuckDuckGoSearchRun())
+
+# # Define Agent with the wrapped tool
+# async def get_agent():
+#     my_agent = Agent(
+#         name="langchain_tool_agent",
+#         model="gemini-2.0-flash-001",
+#         description="Agent to answer questions using TavilySearch.",
+#         instruction="I can answer your questions by searching the internet. Just ask me anything!",
+#         tools=[adk_tavily_tool] # Add the wrapped tool here
+#     )
+#     return my_agent
+
+
+# # step 2 : run the agent
+# async def main(query):
+
+#     # create memory session 
+#     session_serivce = InMemorySessionService()
+#     await session_serivce.create_session(app_name= APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
+    
+#     # get the agent 
+#     root_agent = await get_agent()
+
+#     # create runnner instance
+#     runner = Runner(app_name=APP_NAME, agent = root_agent, session_service=session_serivce)
+
+#     # format the query 
+#     content = types.Content(role = "user", parts= [types.Part(text=query)])
+
+#     print("Running agent with query:", query)
+#     # run the agent 
+#     events = runner.run_async (
+#         new_message = content,
+#         user_id=USER_ID,
+#         session_id=SESSION_ID,
+#     )
+
+
+#     # print the response
+#     async for event in events:
+#         if event.is_final_response():
+#             final_response = event.content.parts[0].text
+#             print("Agent Response:", final_response)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main("Who won he cricket mach between India and Australia on 25th October 2025? Give me detailed statistics"))
